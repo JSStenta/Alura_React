0a2cd6546a5c8007a3aa491258b3206f88f1f621
Juansezhin <jsstenta@outlook.com>
2025-01-20 12:37:02 -0300

    Agregar funcionalidad para actualizar el color de los equipos y mejorar la gestión de colaboradores

--- src/App.js
+++ src/App.js
@@ -38,28 +38,8 @@ function App() {
     nombre: "Jose Gonzalez",
     puesto: "Dev FullStack"
   }])
-  //Ternario --> condicion ? seMuestra : noSeMuestra
-  // condicion && seMuestra
 
-  const cambiarMostrar = () => {
-    actualizarMostrar(!mostrarFormulario)
-  }
-
-  //Registrar colaborador
-
-  const registrarColaborador = (colaborador) => {
-    console.log("Nuevo colaborador", colaborador)
-    //Spread operator
-    actualizarColaboradores([...colaboradores, colaborador])
-  }
-
-  //Eliminar colaborador
-  const eliminarColaborador = () => {
-    console.log("Eliminar colaborador")
-  }
-
-  //Lista de equipos
-  const equipos = [
+  const [equipos, actualizarEquipos] = useState([
     {
       titulo: "Programación",
       colorPrimario: "#57C278",
@@ -95,11 +75,41 @@ function App() {
       colorPrimario: "#FF8A29",
       colorSecundario: "#FFEEDF"
     }
-  ]
+  ])
+
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario)
+  }
+
+  //Registrar colaborador
+  const registrarColaborador = (colaborador) => {
+    console.log("Nuevo colaborador", colaborador)
+    //Spread operator
+    actualizarColaboradores([...colaboradores, colaborador])
+  }
+
+  //Eliminar colaborador
+  const eliminarColaborador = () => {
+    console.log("Eliminar colaborador")
+  }
+
+  //Actualizar color de equipo
+  const actualizarColor = (color, titulo) => {
+    console.log("Actualizar: ", color, titulo)
+    const equiposActualizados = equipos.map((equipo) => {
+      if (equipo.titulo === titulo) {
+        equipo.colorPrimario = color
+      }
+
+      return equipo
+    })
+
+    actualizarEquipos(equiposActualizados)
+  }
+
   return (
     <div>
       <Header />
-      {/* {mostrarFormulario ? <Formulario /> : <></>} */}
       {
         mostrarFormulario && <Formulario
           equipos={equipos.map((equipo) => equipo.titulo)}
@@ -115,6 +125,7 @@ function App() {
           key={equipo.titulo}
           colaboradores={colaboradores.filter(colaborador => colaborador.equipo === equipo.titulo)}
           eliminarColaborador={eliminarColaborador}
+          actualizarColor={actualizarColor}
         />
         )
       }
